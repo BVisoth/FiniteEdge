c38380247198a06652fa8a67cf4d58587da8a1fa
Cel-Johnson <92333735+Cel-Johnson@users.noreply.github.com>
2022-01-27 12:43:08 -0800

    Boss scripts and projectiles

--- FiniteEdge_Mobile/Assets/Scripts/BossPreset.cs
+++ FiniteEdge_Mobile/Assets/Scripts/BossPreset.cs
@@ -5,19 +5,67 @@ using UnityEngine;
 public class BossPreset : MonoBehaviour
 {
     public float counter;
-   static public int ran1;
+    public float acounter;
+    static public int ran1;
     public int apple;
     public Transform[] positions;
     public GameObject ball;
+    public float atime;
+    [SerializeField] public int atimeset;
+    [SerializeField] public int sscount;
+    public int scount;
+    public bool atimecheck;
+    public bool scountcheck;
+    [SerializeField] static public int bigmanhealth;
     // Start is called before the first frame update
     void Start()
     {
-        
+        atime = atimeset / singleton.diA;
     }
 
     // Update is called once per frame
     void Update()
-    
+
+    {
+        if (scount > 0)
+        {
+            Shoot();
+        }
+        else
+        {
+            if (atimecheck == false)
+            {
+                atime = atimeset / singleton.diA;
+                atimecheck = true;
+                scountcheck = false;
+            }
+               
+
+           
+        }
+        if (atime > 0)
+        {
+            acounter += Time.deltaTime;
+            if (acounter >= 1)
+            {
+                acounter = 0;
+                atime = atime - 1;
+            }
+        }
+        else if (atime <= 0)
+        {
+            if (scountcheck == false)
+            {
+                scount = sscount * singleton.diA;
+                scountcheck = true;
+                atimecheck = false;
+            }
+
+
+        }
+    }
+
+    private void Shoot()
     {
         counter += Time.deltaTime;
         if (counter >= 1)
@@ -27,8 +75,8 @@ public class BossPreset : MonoBehaviour
             ran1 = apple;
             Vector3 position = positions[apple].position;
             Instantiate(ball, position, Quaternion.identity);
+            scount = scount - 1;
         }
-
     }
 }
 
