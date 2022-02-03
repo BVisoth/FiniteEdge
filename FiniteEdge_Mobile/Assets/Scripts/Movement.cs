3a251cdef1d7f545903901a08b809b466e0eac18
ElCapotan <jaxonostrer@gmail.com>
2022-02-03 12:29:35 -0800

    vgn

--- FiniteEdge_Mobile/Assets/Scripts/Movement.cs
+++ FiniteEdge_Mobile/Assets/Scripts/Movement.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Jaxon_test : MonoBehaviour
+public class Movement : MonoBehaviour
 {
 
     // Update is called once per frame
