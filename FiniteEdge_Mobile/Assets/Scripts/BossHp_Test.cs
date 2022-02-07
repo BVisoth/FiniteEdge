d5c10d5562fe50203f175eb394744c4b60cedb35
Brandon-Beer <brandonbeer068@gmail.com>
2022-02-07 13:53:39 -0800

    attempted to fix animations
    it didn't work

--- FiniteEdge_Mobile/Assets/Scripts/BossHp_Test.cs
+++ FiniteEdge_Mobile/Assets/Scripts/BossHp_Test.cs
@@ -22,17 +22,16 @@ public class BossHp_Test : MonoBehaviour
     {
         if (Input.GetKeyDown(KeyCode.UpArrow))
         {
-            animator.SetInteger("Anim", 3);
             HitPoints -= 10;
             hpBar.SetHealth(HitPoints, MaxHitpoints);
             Debug.Log("Current Hp: " + HitPoints);
-            animator.SetInteger("Anim", 1);
 
             if (HitPoints <= 0)
             {
                 Destroy(gameObject);
                 SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 2);
             }
+            animator.SetInteger("Anim", 3);
         }
     }
 }
