7b9fee81399d303ce0f0cd2fa6f6c82ac652d49b
BVisoth <92333663+BVisoth@users.noreply.github.com>
2022-02-09 12:24:26 -0800

    Additional swipe testing

--- FiniteEdge_Mobile/Assets/Scripts/Swipe_Test.cs
+++ FiniteEdge_Mobile/Assets/Scripts/Swipe_Test.cs
@@ -4,11 +4,56 @@ using UnityEngine;
 
 public class Swipe_Test : MonoBehaviour
 {
+    public static bool atturn;
+    public int attackd;
+
+    private void Start()
+    {
+        atturn = false;
+    }
+
     void Update()
     {
         if(BossPreset.atime > 0)
         {
-            BossPreset.attk = true;
+            Debug.Log("Atime is over");
+            atturn = true;
         }
+        else
+        {
+            atturn = false;
+        }
+
+        if(atturn == true)
+        {
+            attackd = Random.Range(1, 5);
+        }
+
+
+
+        //This Section is for the directions where the player needs to attack via arrows that will be added. For now the indicators will be in the debug log.
+        switch (attackd)
+        {
+            case 1:
+                Debug.Log("Go Left!");
+                atturn = false;
+                break;
+            case 2:
+                Debug.Log("Go Right!");
+                atturn = false;
+                break;
+            case 3:
+                Debug.Log("Go Up!");
+                atturn = false;
+                break;
+            case 4:
+                Debug.Log("Go Down!");
+                atturn = false;
+                break;
+            default:
+                atturn = false;
+                break;
+        }
+
     }
 }
